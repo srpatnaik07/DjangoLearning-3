a5e52fe344672a2dbd89de14e1778f5ea7546bb3
srpatnaik07 <48448680+srpatnaik07@users.noreply.github.com>
2023-06-19 03:02:01 +0530

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,62 @@
+import requests
+import json
+
+URL = "http://127.0.0.1:8000/studentapi/"
+def get_data(id = None):
+    data={}
+    if id is not None:
+        data = {'id':id}
+    json_data = json.dumps(data)
+    response = requests.get(url=URL, data=json_data)
+    data=response.json()
+    print(data)
+
+
+get_data(4)  
+
+def post_data():
+    data = {
+        'name': 'Chaman Pandey',
+        'roll': 106,
+        'city': 'Buxar'
+    }
+
+    json_data = json.dumps(data)
+    r = requests.post(url=URL,data = json_data)
+    data = r.json()
+    print(data)
+
+# post_data()
+
+def update_data():
+    data= {
+        'id': 8,
+        'name': 'Shubham Sahu',
+        'city': 'Bodh Gaya'
+    }
+    json_data= json.dumps(data)
+    r= requests.put(url=URL, data=json_data)  
+    data=r.json
+    print(data)  
+
+update_data()
+
+def delete_data():
+    data = {'id':1}
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL,data = json_data)
+    data = r.json()
+    print(data)
+
+# delete_data()    
+
+
+
+    
+
+
+
+
+
+
+
